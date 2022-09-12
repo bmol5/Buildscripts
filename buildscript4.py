0ce81178761f420304ed49c749e2a2af4765bf75
brayan <bamolina5@outlook.com>
2022-09-12 16:46:56 -0400

    added list of premier league teams to script

--- buildscript4.py
+++ buildscript4.py
@@ -1,7 +1,23 @@
 #!/bin/python3
-import subprocess, requests, json
+import subprocess, requests, json, time
 
-url = 'https://api.football-data.org/v4/teams/61'
+print("This script will store squad information about the English Premier League teams for the current season in a csv file")
+time.sleep(3)
+print("Please select a team from the list below\n")
+time.sleep(3)
+
+pl_tv = {'Arsenal':57,'Aston Villa':58,'Chelsea':61,'Everton':62,'Fulham':63,'Liverpool':64,'Manchester City':65,'Manchester United':66,'Newcastle':67,'Tottenham Hotspurs':73,
+		'Wolverhampton Wanderers':76,'Leicester City':338,'Southampton':340,'Leeds United':341,
+		'Nottingham Forest':351,'Crystal Palace':354,'Brighton & Hove Albion':397,'Brentford':402,'West Ham':563,'Bournemouth':1044}
+
+pl_teams = list(pl_tv.keys())
+
+for teams in pl_teams:
+	print(teams)
+
+team_selected = input("")
+
+url = 'https://api.football-data.org/v4/teams/{}'.format(pl_tv[team_selected])
 
 headers = {
 	"X-Auth-Token": "775d48413ec742418c53e5cf5f650a82",
@@ -10,6 +26,7 @@ headers = {
 	}
 
 response = requests.request("GET", url, headers=headers)
+
 data_json = response.json()
 final_result = json.dumps(data_json, indent=4)
 string_result = str(final_result)
